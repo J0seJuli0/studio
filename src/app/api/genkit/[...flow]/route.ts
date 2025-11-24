86e2fda2f1bc124fdaa889729112e6c6add0f618
Jose Julio Sanchez Cruzado <josejuliosanchezcruzado1@gmail.com>
2025-11-24 00:13:49 +0000

    > Build error occurred [Error: Failed to collect page data for /api/genk

--- src/app/api/genkit/[...flow]/route.ts
+++ src/app/api/genkit/[...flow]/route.ts
@@ -1,5 +1,5 @@
-import { dev } from '@genkit-ai/next';
+import { prod } from '@genkit-ai/next';
 import '@/ai/flows/real-time-route-adjustment';
 import '@/ai/flows/optimize-route-factors';
 
-export const POST = dev();
+export const POST = prod();
